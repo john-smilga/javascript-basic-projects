bc147e9df98e7aed6b427102576c5eabce0a55dd
John <johnsmilga@gmail.com>
2020-04-14 12:20:35 -0700

    sidebar-project-added

---
+++ 4-sidebar/final/app.js
@@ -0,0 +1,15 @@
+const toggleBtn = document.querySelector(".sidebar-toggle");
+const closeBtn = document.querySelector(".close-btn");
+const sidebar = document.querySelector(".sidebar");
+
+toggleBtn.addEventListener("click", function () {
+  if (sidebar.classList.contains("show-sidebar")) {
+    sidebar.classList.remove("show-sidebar");
+  } else {
+    sidebar.classList.add("show-sidebar");
+  }
+});
+
+closeBtn.addEventListener("click", function () {
+  sidebar.classList.remove("show-sidebar");
+});
