767d06f4ff9534ac4b8648fe2ac8d91eb8675299
John <johnsmilga@gmail.com>
2020-04-14 20:31:55 -0700

    setup for top-navbar added

--- 4-sidebar/final/app.js
+++ 4-sidebar/final/app.js
@@ -3,11 +3,12 @@ const closeBtn = document.querySelector(".close-btn");
 const sidebar = document.querySelector(".sidebar");
 
 toggleBtn.addEventListener("click", function () {
-  if (sidebar.classList.contains("show-sidebar")) {
-    sidebar.classList.remove("show-sidebar");
-  } else {
-    sidebar.classList.add("show-sidebar");
-  }
+  // if (sidebar.classList.contains("show-sidebar")) {
+  //   sidebar.classList.remove("show-sidebar");
+  // } else {
+  //   sidebar.classList.add("show-sidebar");
+  // }
+  sidebar.classList.toggle("show-sidebar");
 });
 
 closeBtn.addEventListener("click", function () {
