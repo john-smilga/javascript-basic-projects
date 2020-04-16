542485a7db3ac37ff0c17a9011989f21e34353f8
john-smilga <johnsmilga@gmail.com>
2020-04-16 09:12:39 -0700

    navbar added

---
+++ 4-navbar/final/app.js
@@ -0,0 +1,20 @@
+// classList - shows/gets all classes
+// contains - checks classList for specific class
+// add - add class
+// remove - remove class
+// toggle - toggles class
+
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+  // console.log(links.classList);
+  // console.log(links.classList.contains("random"));
+  // console.log(links.classList.contains("links"));
+  // if (links.classList.contains("show-links")) {
+  //   links.classList.remove("show-links");
+  // } else {
+  //   links.classList.add("show-links");
+  // }
+  links.classList.toggle("show-links");
+});
