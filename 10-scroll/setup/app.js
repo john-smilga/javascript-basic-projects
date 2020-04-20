c93e7b7475a4ee11fc3d03436a37d06f0d509cb5
John <johnsmilga@gmail.com>
2020-04-19 20:02:31 -0700

    10-scroll-project complete

---
+++ 10-scroll/setup/app.js
@@ -0,0 +1,10 @@
+// pageYOffset is a read - only window property that returns the number of pixels the document has been scrolled vertically.
+// slice extracts a section of a string without modifying original string
+//offsetTop - A Number, representing the top position of the element, in pixels
+
+// ********** set date ************
+
+// ********** fixed navbar and back to top ************
+
+// ********** smooth scroll ************
+// select links
