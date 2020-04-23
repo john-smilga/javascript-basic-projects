47645c5a6d48a526d61a34d81890a8f7e646423c
John <johnsmilga@gmail.com>
2020-04-23 12:46:57 -0700

    12-countdown-timer complete

---
+++ 12-countdown-timer/setup/app.js
@@ -0,0 +1,23 @@
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const weekdays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
