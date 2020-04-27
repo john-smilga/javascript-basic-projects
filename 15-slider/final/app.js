c46832b1689fe04080be65130fb7de88a8b4bb2e
John <johnsmilga@gmail.com>
2020-04-27 08:15:39 -0700

    15-button option added to the slider

--- 15-slider/final/app.js
+++ 15-slider/final/app.js
@@ -16,13 +16,30 @@ prevBtn.addEventListener("click", function () {
 });
 
 function carousel() {
-  if (counter === slides.length) {
+  // working with slides
+  // if (counter === slides.length) {
+  //   counter = 0;
+  // }
+  // if (counter < 0) {
+  //   counter = slides.length - 1;
+  // }
+  // working with buttons
+  if (counter < 0) {
     counter = 0;
   }
-  if (counter < 0) {
-    counter = slides.length - 1;
+  if (counter < slides.length - 1) {
+    nextBtn.style.display = "block";
+  } else {
+    nextBtn.style.display = "none";
+  }
+  if (counter > 0) {
+    prevBtn.style.display = "block";
+  } else {
+    prevBtn.style.display = "none";
   }
   slides.forEach(function (slide) {
     slide.style.transform = `translateX(-${counter * 100}%)`;
   });
 }
+
+prevBtn.style.display = "none";
