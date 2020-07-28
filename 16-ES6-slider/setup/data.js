550746aeb0360e5d92c34e94d2aef6e828b82f3d
John <johnsmilga@gmail.com>
2020-07-28 11:16:01 -0700

    ES6 Slider project added

---
+++ 16-ES6-slider/setup/data.js
@@ -0,0 +1,23 @@
+const people = [
+  {
+    img:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/c_scale,w_200/v1595959121/person-1_aufeoq.jpg",
+    name: "peter doe",
+    job: "product manager",
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis? `,
+  },
+  {
+    img:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/c_scale,w_200/v1595959131/person-2_ipcjws.jpg",
+    name: "susan doe",
+    job: "developer",
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+  },
+  {
+    img:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/c_scale,w_200/v1595959131/person-3_rxtqvi.jpg",
+    name: "emma doe",
+    job: "designer",
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+  },
+];
