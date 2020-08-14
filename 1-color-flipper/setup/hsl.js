c94ce86e7ef3d70509f93dca81862ef8c188cbe7
chamannarved <chamannarved@gmail.com>
2020-08-14 21:19:29 +0530

    create a getValues() function

--- 1-color-flipper/setup/hsl.js
+++ 1-color-flipper/setup/hsl.js
@@ -3,3 +3,12 @@
     hsl(0 - 360, 0 -100%,0 - 100%,0 - 1)
 
 */
+
+// get value for saturation and light (0-100)
+function getValues() {
+  let value = [];
+  for (let i = 0; i <= 100; i++) {
+    value.push(i);
+  }
+  return value;
+}
