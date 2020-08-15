fd530f61801a1f25335051278adead4a84660e9f
chamannarved <chamannarved@gmail.com>
2020-08-15 08:41:46 +0530

    setup files added to final for hsl color

---
+++ 1-color-flipper/final/hsl.js
@@ -0,0 +1,35 @@
+/* 
+    hsl(hue,saturation,light,alpha)
+    hsl(0 - 360, 0 -100%,0 - 100%,0 - 1)
+
+*/
+
+// get value for saturation and light (0-100)
+function getValues() {
+  let value = [];
+  for (let i = 0; i <= 100; i++) {
+    value.push(i);
+  }
+  return value;
+}
+
+//get values for hue (0 - 360)
+function getHueValues() {
+  let value = [];
+  for (let i = 0; i <= 360; i++) {
+    value.push(i);
+  }
+  return value;
+}
+
+// get values for alpha
+function getAlphaValues() {
+  return Math.random().toFixed(2);
+}
+// get all values in one array of color
+const hsl = [
+  [getHueValues()],
+  [getValues()],
+  [getValues()],
+  [getAlphaValues(), 1],
+];
