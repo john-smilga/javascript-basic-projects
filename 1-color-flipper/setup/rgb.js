d7c72e447329956012c4af87677dd5267d83967c
chamannarved <chamannarved@gmail.com>
2020-08-15 13:59:40 +0530

    create a functoin for rgb values

--- 1-color-flipper/setup/rgb.js
+++ 1-color-flipper/setup/rgb.js
@@ -3,3 +3,12 @@
     rgb(0 - 255, 0 -255,0 - 255,0 - 1)
 
 */
+
+// get values for red, green and blue (0-255)
+function getValues() {
+  let value = [];
+  for (let i = 0; i <= 255; i++) {
+    value.push(i);
+  }
+  return value;
+}
