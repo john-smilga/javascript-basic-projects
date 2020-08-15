0c1e8a0acbde92d97355f386de0cba2d20039553
chamannarved <chamannarved@gmail.com>
2020-08-15 15:22:15 +0530

    add hard code for testing

--- 1-color-flipper/final/rgb.js
+++ 1-color-flipper/final/rgb.js
@@ -25,5 +25,7 @@ const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
 btn.addEventListener("click", function () {
-  let rgbColor;
+  let rgbColor = "rgb(" + 122 + "," + 124 + "," + 10 + "," + 0.89 + ")";
+
+  console.log(rgbColor);
 });
