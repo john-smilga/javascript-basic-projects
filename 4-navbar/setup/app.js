10152be02299b0657c9a39f3fadc4b99c2b2eaf0
MRFolio <roose.markus@gmail.com>
2020-09-14 22:06:23 +0300

    Add navbar project

--- 4-navbar/setup/app.js
+++ 4-navbar/setup/app.js
@@ -3,3 +3,10 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", () => {
+  links.classList.toggle("show-links");
+});
