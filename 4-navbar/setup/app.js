b5f46520c3b6fd96b7565ee32153da81bca4113e
MRFolio <roose.markus@gmail.com>
2020-09-15 00:43:47 +0300

    navbar with extra functionality

--- 4-navbar/setup/app.js
+++ 4-navbar/setup/app.js
@@ -6,8 +6,28 @@
 
 const navToggle = document.querySelector(".nav-toggle");
 const links = document.querySelector(".links");
+//icon item
+const awesome = document.getElementById("awesome");
 
 navToggle.addEventListener("click", () => {
   links.classList.toggle("show-links");
-  navToggle.innerHTML = <i class="fas fa-bars"></i>;
+  awesome.className === "fas fa-bars"
+    ? (awesome.className = "fas fa-times")
+    : (awesome.className = "fas fa-bars");
 });
+
+// outside click
+/* window.addEventListener("click", (e) => {
+  e.target.classList !== "links"
+    ? links.classList.remove("show-links")
+    : false; /* == links ? links.classList.remove("show-links") : false; */
+
+/* navToggle.addEventListener("click", () => {
+  if (links.classList.contains("show-links")) {
+    links.classList.remove("show-links");
+    navToggle.innerHTML = <i class="fas fa-bars"></i>;
+  } else {
+    links.classList.add("show-links");
+    navToggle.innerHTML = <i class="fas fa-times"></i>;
+  }
+}); */
