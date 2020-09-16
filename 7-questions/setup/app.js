8f456b975f3536bae34b83bd719b84401e05d064
MRFolio <roose.markus@gmail.com>
2020-09-16 23:54:53 +0300

    feature(questions): add new feature for icon TEST

--- 7-questions/setup/app.js
+++ 7-questions/setup/app.js
@@ -1,16 +1,27 @@
 //using selectors inside the element
 const questions = document.querySelectorAll(".question");
+const awesomeIcons = document.getElementById("awesome");
+
+/* function toggleIcon(element, awesomeIconName1, awesomeIconName2) {
+  element.className === awesomeIconName1
+    ? (element.className = awesomeIconName2)
+    : (element.className = awesomeIconName1);
+} */
 
 questions.forEach((question) => {
   const btn = question.querySelector(".question-btn");
-  console.log(btn);
-  btn.addEventListener("click", (params) => {
+  btn.addEventListener("click", () => {
     questions.forEach((item) => {
       item !== question ? item.classList.remove("show-text") : false;
     });
     question.classList.toggle("show-text");
+    /* toggleIcon(awesome, "fas fa-chevron-down", "fas fa-chevron-up"); */
   });
 });
+
+document.addEventListener("keydown", (e) =>
+  e.key === "Escape" ? question.classList.remove("show-sidebar") : false
+);
 
 // traversing the dom
 
