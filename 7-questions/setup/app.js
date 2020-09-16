7da556c175e5c96d26e8503c0b70b0bd27021d0a
MRFolio <roose.markus@gmail.com>
2020-09-16 21:49:25 +0300

    Feature(Questions): Add extra features

--- 7-questions/setup/app.js
+++ 7-questions/setup/app.js
@@ -1,2 +1,10 @@
 //using selectors inside the element
 // traversing the dom
+
+const btns = document.querySelectorAll(".question-btn");
+btns.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    const question = e.currentTarget.parentElement.parentElement;
+    question.classList.toggle("show-text");
+  });
+});
