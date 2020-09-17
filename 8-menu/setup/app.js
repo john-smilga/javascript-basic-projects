9e4c02e3acbf0fe5d71ff990debc620f4378c3de
MRFolio <roose.markus@gmail.com>
2020-09-17 22:43:21 +0300

    Feature(menu): Add completed menu project

--- 8-menu/setup/app.js
+++ 8-menu/setup/app.js
@@ -71,4 +71,68 @@ const menu = [
     img: "./images/item-9.jpeg",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
+  {
+    id: 10,
+    title: "bison steak",
+    category: "dinner",
+    price: 22.99,
+    img: "./images/item-10.jpeg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
 ];
+const sectionCenter = document.querySelector(".section-center");
+const container = document.querySelector(".btn-container");
+
+/* load items */
+document.addEventListener("DOMContentLoaded", () => {
+  displayMenuItems(menu);
+  displayMenuButton();
+});
+
+const displayMenuItems = (menuItems) => {
+  let displayMenu = menuItems.map((item) => {
+    return `<article class="menu-item">
+    <img src=${item.img} class="photo" alt=${item.title} />
+    <div class="item-info">
+      <header>
+        <h4>${item.title}</h4>
+        <h4 class="price">$${item.price}</h4>
+      </header>
+      <p class="item-text">
+        ${item.desc}
+      </p>
+    </div>
+  </article>`;
+  });
+  displayMenu = displayMenu.join("");
+  sectionCenter.innerHTML = displayMenu;
+};
+
+const displayMenuButton = () => {
+  const categories = menu.reduce(
+    (values, item) => {
+      if (!values.includes(item.category)) {
+        values.push(item.category);
+      }
+      return values;
+    },
+    ["all"]
+  );
+  const categoryBtns = categories
+    .map(
+      (category) =>
+        `<button class="filter-btn" type="button" data-id="${category}">${category}</button>`
+    )
+    .join("");
+  container.innerHTML = categoryBtns;
+  const filterBtns = container.querySelectorAll(".filter-btn");
+  filterBtns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      const category = e.currentTarget.dataset.id;
+      const menuCategory = menu.filter((item) => item.category === category);
+      category === "all"
+        ? displayMenuItems(menu)
+        : displayMenuItems(menuCategory);
+    });
+  });
+};
