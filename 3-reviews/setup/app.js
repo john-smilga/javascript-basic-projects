54d6d145f06bea5226aadec50e10a2d18e145977
Shadab Ali <shadabali604@gmail.com>
2020-10-04 10:01:35 +0530

    Decreasing is not working

--- 3-reviews/setup/app.js
+++ 3-reviews/setup/app.js
@@ -1,74 +1,67 @@
 // local reviews data
-const reviews = [
-  {
+const reviews = [{
     id: 1,
     name: "susan smith",
     job: "web developer",
-    img:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
-    text:
-      "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+    text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
   },
   {
     id: 2,
     name: "anna johnson",
     job: "web designer",
-    img:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
-    text:
-      "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+    text: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
   },
   {
     id: 3,
     name: "peter jones",
     job: "intern",
-    img:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
-    text:
-      "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+    text: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
   },
   {
     id: 4,
     name: "bill anderson",
     job: "the boss",
-    img:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
-    text:
-      "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+    text: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
   },
 ];
-function createUI(){
-  reviews.forEach((review) => {
-  let container = document.createElement('div');
-  container.setAttribute('class', 'container');
-  let title = document.createElement('div');
-  title.setAttribute('class', 'title');
-  let h2 = document.createElement('h2');
-  let underline = document.createElement('div');
-  underline.setAttribute('class', 'underline');
-  title.appendChild(h2, underline);
-  let article = document.querySelector('article');
-  article.setAttribute('class', 'review');
-  let imgCon = document.createElement('div');
-  imgCon.setAttribute('class', 'img-container');
-  let img = document.createElement('img');
-  screen.scr = review.img;
-  img.setAttribute('id', 'person-img');
-  let h4 = document.createElement('h4');
-  h4.setAttribute('id', 'author');
-  let job = document.createElement('p');
-  job.setAttribute('id', 'job');
-  job.innerText = review.job;
-  let info= document.createElement('p');
-  info.setAttribute('id', 'info');
-  info.innerText = review.info;
-  imgCon.append(h4, job);
-  let btncon = document.createElement('div');
-  btncon.setAttribute('class', 'button-container');
-  
 
-  article(imgCon, info);
+let index = [];
+let nextBtn = document.querySelector('.next-btn');
+let preBtn = document.querySelector('.prev-btn');
+let img = document.querySelector('#person-img');
+let author = document.querySelector('#author');
+let job = document.querySelector('#job');
+let info = document.querySelector('#info');
 
-    
-  })
+function ui() {
+  img.src = reviews[index].img;
+  author.innerHTML = reviews[index].name;
+  job.innerHTML = reviews[index].job;
+  info.innerHTML = reviews[index].text;
 }
+
+function CardChange() {
+   ui()
+  console.log(index, reviews.length-1);
+  if (index >= reviews.length-1) {
+    index = -1
+  }
+   index.push(index);
+}
+
+function ChangeCard() {
+  ui();
+  index = index - 1;
+  console.log(index);
+}
+
+console.log(index);
+
+nextBtn.addEventListener('click', CardChange);
+
+preBtn.addEventListener('click', ChangeCard);
+console.log(preBtn);
