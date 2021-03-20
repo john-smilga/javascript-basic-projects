f4a0d1de8bd907c973431227eaef69e045829521
mindfudge <narayanegn@gmail.com>
2021-03-20 14:52:02 -0300

    changes

--- 1-color-flipper/setup/hex.js
+++ 1-color-flipper/setup/hex.js
@@ -1,3 +1,18 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+// #letters and numbers, randomly gives us colors
 const btn = document.getElementById("btn");
-const btn = document.querySelector(".color");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function() {
+    let hexColor = "#";
+    for (let i = 0; i < 6; i++) {
+        hexColor += hex[getRandomNumber()];
+    }
+
+    color.textContent = hexColor;
+    document.body.style.backgroundColor = hexColor; 
+});
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * hex.length);
+}
