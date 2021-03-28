d040bb8c723e6946e2eeba70ccd671f75b897a5e
Yudhvir Singh <yuvi@mac.local>
2021-03-28 16:04:39 -0400

    Finish up on color-flipper

--- 01-color-flipper/final/app.js
+++ 01-color-flipper/final/app.js
@@ -1,1 +1,14 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+function getRandomValueFromArray(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+const colorSpan = document.querySelector(".color");
+const main = document.querySelector("main");
+
+document.querySelector("#btn").addEventListener("click", () => {
+  const newColor = getRandomValueFromArray(colors);
+  colorSpan.textContent = newColor;
+  document.body.style.backgroundColor = newColor;
+});
--- 01-color-flipper/final/hex.js
+++ 01-color-flipper/final/hex.js
@@ -1,1 +1,22 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+function getRandomValueFromArray(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+function computeNewColor() {
+  let resultString = "#";
+  for (let i = 0; i < 6; i++) {
+    resultString += getRandomValueFromArray(hex);
+  }
+  return resultString;
+}
+
+const colorSpan = document.querySelector("#color-span");
+const main = document.querySelector("main");
+
+document.querySelector("#btn").addEventListener("click", () => {
+  const newColor = computeNewColor();
+  colorSpan.innerHTML = newColor;
+  document.body.style.backgroundColor = newColor;
+});
