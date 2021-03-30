aafeffb420378f35db1bc0628fe76298242b32f4
Mattia Zanella <mattia_zanella@icloud.com>
2021-03-30 09:55:30 +0200

    Useless preloader logic. Replaced with poster attribute.

--- 9-video/final/app.js
+++ 9-video/final/app.js
@@ -10,10 +10,3 @@ btn.addEventListener("click", function () {
     video.play();
   }
 });
-
-// preloader
-const preloader = document.querySelector(".preloader");
-
-window.addEventListener("load", function () {
-  preloader.classList.add("hide-preloader");
-});
