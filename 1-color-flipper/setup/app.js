d14e7c803da9fd2e44162d86658a51831c248378
Matheus Almeida <matheusalmeida016@gmail.com>
2021-03-31 09:56:08 -0400

    Working on simple colors

--- 1-color-flipper/setup/app.js
+++ 1-color-flipper/setup/app.js
@@ -1,1 +1,9 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.querySelector('#btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', () => {
+  const randomNumber = Math.round(Math.random() * 3);
+  
+});
