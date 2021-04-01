7249e1a73158b90610431c768f097f636b5dca45
Matheus Almeida <matheusalmeida016@gmail.com>
2021-04-01 05:52:16 -0400

    Finish hex js code

--- 1-color-flipper/setup/app.js
+++ 1-color-flipper/setup/app.js
@@ -1,9 +1,14 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
-
-const btn = document.querySelector('#btn');
+const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
 
 btn.addEventListener('click', () => {
-  const randomNumber = Math.round(Math.random() * 3);
+  const randomNumber = getRandomNumber();
   
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
 });
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
--- 1-color-flipper/setup/hex.js
+++ 1-color-flipper/setup/hex.js
@@ -1,1 +1,26 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', () => {
+  const randomColor = getRandomColor();
+  
+  document.body.style.backgroundColor = randomColor;
+  color.textContent = randomColor;
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * hex.length);
+}
+
+function getRandomColor() {
+  let color = '#';
+  let number;
+  
+  for (let i = 0; i < 6; i++) {
+    number = getRandomNumber();
+    color += `${hex[number]}`;
+  }
+  
+  return color;
+}
