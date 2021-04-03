5095cd15f55fd0bdc4e5fc3c1d294136fc43aac7
Matheus Almeida <matheusalmeida016@gmail.com>
2021-04-03 12:32:11 -0400

    Add reviews project

--- 3-reviews/app.js
+++ 3-reviews/app.js
@@ -37,3 +37,59 @@ const reviews = [
       "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
   },
 ];
+
+const img = document.getElementById('person-img');
+const author = document.getElementById('author');
+const job = document.getElementById('job');
+const info = document.getElementById('info');
+
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+const randomBtn = document.querySelector('.random-btn');
+
+let currentItem = 0;
+
+window.addEventListener('DOMContentLoaded', () => showPerson(currentItem));
+
+function showPerson(person) {
+  const item = reviews[person];
+  
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+}
+
+nextBtn.addEventListener('click', () => {
+  currentItem++;
+  
+  if (currentItem > reviews.length -1) {
+    currentItem = 0;
+  }
+  
+  showPerson(currentItem);
+});
+
+prevBtn.addEventListener('click', () => {
+  currentItem--;
+  
+  if (currentItem < 0) {
+    currentItem = reviews.length - 1;
+  }
+  
+  showPerson(currentItem);
+});
+
+function randomPerson() {
+  const randomNum = Math.floor(Math.random() * reviews.length);
+  
+  if (currentItem === randomNum) {
+    return randomPerson();
+  }
+  
+  currentItem = randomNum;
+  
+  showPerson(currentItem);
+}
+
+randomBtn.addEventListener('click', randomPerson);
