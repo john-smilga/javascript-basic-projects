657bb74072bdfa614031605936ac1b9176c24ab4
Matheus Almeida <matheusalmeida016@gmail.com>
2021-04-06 00:59:14 -0400

    Add modal project

--- 6-modal/app.js
+++ 6-modal/app.js
@@ -1,4 +1,21 @@
-// select modal-btn,modal-overlay,close-btn
-// listen for click events on modal-btn and close-btn
-// when user clicks modal-btn add .open-modal to modal-overlay
-// when user clicks close-btn remove .open-modal from modal-overlay
+const modalBtn = document.querySelector('.modal-btn');
+const modalOverlay = document.querySelector('.modal-overlay');
+const closeBtn = document.querySelector('.close-btn');
+
+function modal(open = true) {
+  if (open) {
+    modalOverlay.classList.add('open-modal');
+  } else {
+    modalOverlay.classList.remove('open-modal');
+  }
+}
+
+modalOverlay.addEventListener('click', (e) => {
+  if (!e.target.classList.contains('modal-overlay'))
+    return;
+    
+  modalOverlay.classList.remove('open-modal');
+});
+
+modalBtn.addEventListener('click', modal);
+closeBtn.addEventListener('click', () => modal(false));
