8252de89fee6d5f770c4c9cc91eac217cc75a0ad
cedis81 <ccchom11@gmail.com>
2021-05-03 14:02:32 -0400

    Complete basic color flipper project
    * Update index.html to contain button
    * Update app.js to add event listener click
    * Update app.js to randomize four pre-set colors
    * Update hex.html to contain button
    * Update hex.js to add event listener click
    * Update hex.js to randomize hex color

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,14 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', function() {
+  // get random numbers 0-3 to access colors array via index
+  const randomNumber = getRandomNumber();
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber]
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
