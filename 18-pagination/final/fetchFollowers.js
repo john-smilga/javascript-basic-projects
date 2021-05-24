a5ac96854564a74435423a5904f4c22a9ece2f5e
Matheus Almeida <matheusalmeida016@gmail.com>
2021-05-23 23:01:48 -0400

    HTML setup

--- 18-pagination/final/fetchFollowers.js
+++
@@ -1,9 +0,0 @@
-const url = 'https://api.github.com/users/john-smilga/followers?per_page=100'
-
-const fetchFollowers = async () => {
-  const response = await fetch(url)
-  const data = await response.json()
-  return data
-}
-
-export default fetchFollowers
