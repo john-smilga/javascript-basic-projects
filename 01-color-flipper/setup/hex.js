cdde669b46d8f810c986e60a0f08ed06e7501c51
martin visca <martin.visca@gmail.com>
2021-08-17 19:45:18 +0200

    fully random implementation, body stayle and document > element handling

--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,14 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', function() {
+  let color_hash = '#';
+  for ( let i = 0; i < 6; i++ ) {
+    let randomNumber = Math.floor(Math.random() * hex.length);
+    color_hash += hex[randomNumber];
+  }
+  color.textContent = color_hash;
+  document.body.style.backgroundColor = color_hash;
+});
