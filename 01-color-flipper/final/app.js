88a94586ff98ed1c14b8c8b0ad09ee02708fa6ef
Owen Eldridge <oweneldridge7@gmail.com>
2022-02-05 22:57:11 -0500

    1-3

--- 01-color-flipper/final/app.js
+++ 01-color-flipper/final/app.js
@@ -1,8 +1,8 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
-const btn = document.getElementById("btn");
-const color = document.querySelector(".color");
+const colors = ['green', 'red', 'rgba(133,122,200)', '#f15025'];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
 
-btn.addEventListener("click", function () {
+btn.addEventListener('click', function () {
   const randomNumber = getRandomNumber();
   // console.log(randomNumber);
 
--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,18 @@
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+// #f15025
+
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', () => {
+  let hexColor = '#';
+  const hexValues = 6;
+  for (let i = 0; i < hexValues; i++) {
+    hexColor += hex[getRandomNumber()];
+  }
+
+  color.textContent = hexColor;
+  document.body.style.backgroundColor = hexColor;
+});
+
+const getRandomNumber = () => Math.floor(Math.random() * hex.length);
