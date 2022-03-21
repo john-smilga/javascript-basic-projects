7fe975b2f819900a506a96837e67c411526069e1
Max <89795178+maxmandia@users.noreply.github.com>
2022-03-21 19:04:25 -0400

    Completed the project with minimal assistance.

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,17 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const color = document.querySelector(".color");
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", function () {
+  //click button and cycle through random colors in the array. Change background color and text
+  const randomNumber = getRandomNumber();
+  console.log(randomNumber);
+
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
