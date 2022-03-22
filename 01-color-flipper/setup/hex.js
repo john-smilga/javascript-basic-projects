54e61bce43a89724939336dcbfb1cc492e9201b3
Max <89795178+maxmandia@users.noreply.github.com>
2022-03-21 22:14:56 -0400

    Completed all of color flipper with a little help.

--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,22 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const color = document.querySelector(".color");
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", function () {
+  //click button and create a random hex number.
+  let hexColor = "#";
+
+  for (let i = 0; i < 6; i++) {
+    hexColor += hex[hexRandom()];
+  }
+
+  document.body.style.backgroundColor = hexColor;
+  color.textContent = hexColor;
+});
+
+function hexRandom() {
+  return Math.floor(Math.random() * hex.length);
+}
+
+console.log(hexRandom);
