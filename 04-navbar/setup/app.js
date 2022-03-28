d4d329ef0b5d06d85ee3faa15f0e0953788150a8
Max <89795178+maxmandia@users.noreply.github.com>
2022-03-27 21:16:58 -0400

    Completed navbar practice.

--- 04-navbar/setup/app.js
+++ 04-navbar/setup/app.js
@@ -3,3 +3,14 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const icon = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+icon.addEventListener("click", function () {
+  if (links.classList.contains("show-links")) {
+    links.classList.remove("show-links");
+  } else {
+    links.classList.add("show-links");
+  }
+});
