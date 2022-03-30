b74482d1f55052f4ce046b504dd403a6ab61c430
Max <89795178+maxmandia@users.noreply.github.com>
2022-03-30 16:31:14 -0400

    Completed review project with a little help.

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,2 +1,11 @@
 //using selectors inside the element
 // traversing the dom
+
+const btns = document.querySelectorAll(".question-btn");
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const question = e.currentTarget.parentElement.parentElement;
+    question.classList.toggle("show-text");
+  });
+});
