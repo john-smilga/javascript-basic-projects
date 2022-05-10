7a66ef7d933e6df61cbcc2f81b5a00d0832e103c
ktibi <ktibi@groupeseb.com>
2022-05-10 11:27:40 +0200

    remove Comment of 04

--- 04-navbar/final/app.js
+++ 04-navbar/final/app.js
@@ -8,13 +8,6 @@ const navToggle = document.querySelector(".nav-toggle");
 const links = document.querySelector(".links");
 
 navToggle.addEventListener("click", function () {
-  // console.log(links.classList);
-  // console.log(links.classList.contains("random"));
-  // console.log(links.classList.contains("links"));
-  // if (links.classList.contains("show-links")) {
-  //   links.classList.remove("show-links");
-  // } else {
-  //   links.classList.add("show-links");
-  // }
+  
   links.classList.toggle("show-links");
 });
