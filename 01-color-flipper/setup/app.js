202f659d4921d3123b8e69eb7b1c2559f1bd7fea
navneetyadav <navneet.yadav@contify.com>
2022-07-18 00:05:43 +0530

    color flipper with no color repeat

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,16 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+var btn = document.getElementById("btn");
+var textColor = document.querySelector(".container .color");
+var prevNumber = randomNumber = -1;
+
+btn.addEventListener("click", function() {
+    randomNumber = getRandomColor();
+    document.body.style.backgroundColor = colors[randomNumber];
+    textColor.textContent = colors[randomNumber];
+    prevNumber = randomNumber;
+});
+
+function getRandomColor() {
+    randomNumber = Math.floor(Math.random() * colors.length);
+    return randomNumber == prevNumber ? randomNumber == colors.length - 1 ? --randomNumber : ++randomNumber : randomNumber;
+}
