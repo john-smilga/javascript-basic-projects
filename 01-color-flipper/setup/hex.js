05479acd0b08d59bce1753c1d8094dcdc717194d
navneetyadav <navneet.yadav@contify.com>
2022-07-18 00:57:20 +0530

    hex code generated

--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,16 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+var btn = document.getElementById("btn");
+var textColor = document.querySelector(".container .color");
+
+btn.addEventListener("click", function() {
+    var colors = "#";
+    for(let i = 0; i < 6; i++) {
+        colors += hex[getRandomColor()];
+    }
+    document.body.style.backgroundColor = colors;
+    textColor.textContent = colors;
+});
+
+function getRandomColor() {
+    return Math.floor(Math.random() * hex.length);
+}
