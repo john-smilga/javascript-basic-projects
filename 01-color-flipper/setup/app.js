045f7524c60ecd65921e160a7dcfbd1e50368774
nqhuy137 <nquochuy137@gmail.com>
2022-09-12 22:25:30 +0700

    Added coloer changing features

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,7 +1,18 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 
-// This change is to test git repositories  
-const name  = {
-    first: "Nguyen",
-    last: "Huy"
+const getRandomColor = () => {
+    let color = colors[Math.floor(Math.random() * colors.length)];
+    return color;
 }
+
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+console.log(color);
+
+btn.addEventListener('click', () => {
+    let randomColor = getRandomColor();
+
+    document.body.style.backgroundColor = randomColor;
+    color.textContent = randomColor;
+});
+
--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,2 +1,13 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 // testing
+
+const btn = document.getElementById('btn');
+console.log(btn);
+
+function getRandomColor(){
+    
+}
+
+btn.addEventListener('click', function() {
+    
+ })
