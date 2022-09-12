5b3b011a4602f27c8bf58431bfa0ef5d388b9c68
nqhuy137 <nquochuy137@gmail.com>
2022-09-12 22:30:31 +0700

    Fixed hex.js

--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -2,12 +2,20 @@ const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 // testing
 
 const btn = document.getElementById('btn');
-console.log(btn);
+const color = document.querySelector('.color');
+console.log(color);
 
 function getRandomColor(){
-    
+    let hexColor = '#';
+    for (let i = 0; i < 6; i++) {
+        hexColor += hex[Math.floor(Math.random() * hex.length)];
+    }
+    return hexColor;
 }
 
 btn.addEventListener('click', function() {
-    
+    const hexColor = getRandomColor();
+
+    document.body.style.backgroundColor = hexColor;
+    color.textContent = hexColor;
  })
