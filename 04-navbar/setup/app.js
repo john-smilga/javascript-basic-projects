135366d9d985d0abc3f4a5e9cbae83dc0294dc9d
Preston Hogaboam <pjhogaboam@gmail.com>
2022-10-03 15:10:10 +0200

    implemented a toggle nav bar feature using javascript

--- 04-navbar/setup/app.js
+++ 04-navbar/setup/app.js
@@ -3,3 +3,12 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const navToggle = document.querySelector('.nav-toggle');
+const links = document.querySelector('.links');
+
+navToggle.addEventListener('click', function() {
+  // console.log(links.classList);
+  // console.log(links.classList.contains('links'));
+ links.classList.toggle('show-links');
+});
