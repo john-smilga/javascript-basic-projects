aa4d49b2a23d6c210eb555a6dbc39574058d73ee
Muluken Mekuriya <mulerthegrete@gmail.com>
2022-10-19 20:12:37 -0400

    Simple color changes

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,14 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.getElementById("btn")
+const colorText = document.querySelector(".color")
+
+btn.addEventListener("click", function(){
+    const randomNumber = getRandomNumber();
+console.log(randomNumber);
+    document.body.style.backgroundColor = colors[randomNumber]
+});
+
+const getRandomNumber = ()  => {
+    return Math.floor(Math.random()*colors.length)
+}
