328594551d65a8d57de44e3a3b2559328df3c6ef
sheik gulfaan <sheikgulf01@gmail.com>
2022-11-11 15:23:25 +0530

    color flipper done

--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,17 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById('btn');
+const value = document.querySelector('.color');
+
+btn.addEventListener('click', e => {
+    let hexColor = '#';
+    for (let i = 0; i < 6; i++) {
+        hexColor += hex[getRandomNumber()];
+    }
+
+    document.body.style.backgroundColor = hexColor;
+    value.textContent = hexColor;
+})
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * hex.length);
+}
