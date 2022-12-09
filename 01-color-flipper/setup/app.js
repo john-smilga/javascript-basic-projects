21952a452a4816adabb9ed6318075246b16a4886
Daniel G <yanimames>
2022-12-09 08:09:41 -0600

    updated modifications based on comments of Luis Santos

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -5,7 +5,6 @@ const color = document.querySelector(".color");
 btn.addEventListener("click", function () {
 // get random number between 0-3 colors
     const randomNumber = getRandomNumber();
-    console.log(randomNumber)
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber]
 });
--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -3,13 +3,13 @@ const btn = document.getElementById("btn")
 const color = document.querySelector(".color")
 
 btn.addEventListener('click', function() {
-let hexColor = '#';
-for (let i = 0; i < 6; i++){
-    hexColor += hex[getRandomNumber()]
-}
-color.textContent = hexColor;
-document.body.style.backgroundColor = hexColor;
-} )
+    let hexColor = '#';
+    for (let i = 0; i < 6; i++){
+        hexColor += hex[getRandomNumber()]
+    }
+    color.textContent = hexColor;
+    document.body.style.backgroundColor = hexColor;
+})
 
 function getRandomNumber() {
     return Math.floor(Math.random()*hex.length)
