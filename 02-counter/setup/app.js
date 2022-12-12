7365467420d8367bd835c75fecc4b04aa7db7c97
Daniel G <yanimames>
2022-12-12 06:22:47 -0600

    changes based on comments

--- 02-counter/setup/app.js
+++ 02-counter/setup/app.js
@@ -6,25 +6,24 @@ const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn")
 
 btns.forEach(function (btn) {
- btn.addEventListener("click", function(e){
-    const styles = e.currentTarget.classList;
-    if(styles.contains("decrease")){
-        count--;
-    }
-    else if(styles.contains('increase')){
-        count++;
-    } else {
-        count = 0;
-    }
-    if (count > 0 ) {
-        value.style.color = "green";
-    }
-    if (count < 0) {
-        value.style.color = "red";
-    }
-    if (count === 0) {
-        value.style.color = "#222"
-    }
+    btn.addEventListener("click", function(e){
+        const styles = e.currentTarget.classList;
+        if (styles.contains("decrease")) {
+           count--;
+        } else if (styles.contains('increase')) {
+            count++;
+        } else {
+            count = 0;
+        }
+        if (count > 0 ) {
+            value.style.color = "green";
+        }
+        if (count < 0) {
+            value.style.color = "red";
+        }
+        if (count === 0) {
+            value.style.color = "#222"
+        }
     value.textContent = count; 
- });
+   });
 });
