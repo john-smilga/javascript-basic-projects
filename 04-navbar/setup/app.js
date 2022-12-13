30966ab7e070aca0d188971fda10abd93e0116a3
Daniel G <yanimames>
2022-12-13 07:50:14 -0600

    nav bar example

--- 04-navbar/setup/app.js
+++ 04-navbar/setup/app.js
@@ -3,3 +3,16 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const navToggleBtn = document.querySelector('.nav-toggle')
+const ulLinks = document.querySelector('.links')
+
+navToggleBtn.addEventListener('click', function(){
+   // if (ulLinks.classList.contains('show-links')) {
+   //     ulLinks.classList.remove('show-links')
+   // } 
+    //else {
+    //    ulLinks.classList.add('show-links')
+   // }
+    ulLinks.classList.toggle("show-links");
+})
