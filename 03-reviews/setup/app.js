06a649d39da45edeb7dbff40eccf7d51a648fea2
Daniel G <yanimames>
2022-12-13 07:02:11 -0600

    reviews example, prev, next and random button examples

--- 03-reviews/setup/app.js
+++ 03-reviews/setup/app.js
@@ -37,3 +37,78 @@ const reviews = [
       "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
   },
 ];
+
+
+// selecy item
+const img = document.getElementById("person-img")
+const author = document.getElementById("author")
+const job = document.getElementById("job")
+const info = document.getElementById("info")
+
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const randomBtn = document.querySelector(".random-btn")
+
+//set starting item
+let currentItem = 0;
+
+// load initial item
+window.addEventListener("DOMContentLoaded", function () {
+      const item = reviews[currentItem];
+          img.src = item.img;
+          author.textContent = item.name;
+          job.textContent = item.job;
+          info.textContent = item.text;
+});
+
+// show person based on item
+
+function showPerson(person){
+  const item = reviews[person];
+  img.src = item.img;
+  //reviews example 
+  author.textContent = `reviews[${currentItem}] ${item.name}`;
+  job.textContent = item.job;
+  info.textContent = item.text;
+}
+
+// show next person 
+nextBtn.addEventListener("click", function () {
+    currentItem++;
+    if (currentItem > (reviews.length - 1)) {
+      currentItem = 0
+    }
+    showPerson(currentItem);
+})
+
+// show prev person
+prevBtn.addEventListener("click", function () {
+    currentItem--;
+        if (currentItem < 0) {
+          currentItem = reviews.length - 1
+        } 
+    showPerson(currentItem);
+})
+
+// show random person
+randomBtn.addEventListener("click", function () {
+    // get random person between 0-3 sujetos
+   currentItem = getRandomPerson();
+       showPerson(currentItem);
+    });
+    
+   function getRandomPerson() { 
+        return Math.floor(Math.random() * reviews.length);
+    }
+
+// show random person 2 
+function getRandomPerson() {
+  const random = Math.random()
+  console.log(`Random = ${random}`)
+  const indiceRandom = random*reviews.length
+  console.log(`indiceRandom = ${indiceRandom}`)
+  const floor = Math.floor(indiceRandom)
+  console.log(`redondeado = ${floor}`)
+  return floor
+}
+
