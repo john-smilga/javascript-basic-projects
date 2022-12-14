93fbcd5bedb77303f04a67ceb96030ae1a8265a9
Daniel G <yanimames>
2022-12-14 08:05:56 -0600

    Me atore, estaba usando de ejemplo el sidebar example

--- 06-modal/setup/app.js
+++ 06-modal/setup/app.js
@@ -1,4 +1,33 @@
 // select modal-btn,modal-overlay,close-btn
+const modalBtn = document.querySelector('.modal-btn')
+const closeBtn = document.querySelector('.close-btn')
+const modalOverlay = document.querySelector('.modal-overlay')
+const modalContainer = document.querySelector('.modal-container')
+
+
 // listen for click events on modal-btn and close-btn
+modalBtn.addEventListener("click", function () {
+    console.log (modalBtn)
+    modalBtn.classList.add("show-modalContainer")
+});
+
+closeBtn.addEventListener("click", function () {
+    modalOverlay.classList.remove(show-modalOverlay)
+})
+
 // when user clicks modal-btn add .open-modal to modal-overlay
+if (modalOverlay.classList.contains('show-modalOverlay')) {
+    modalOverlay.classList.remove('show-modalOverlay')
+}
+else {
+modalOverlay.classList.add("show-modal-overlay")
+}
+
+
 // when user clicks close-btn remove .open-modal from modal-overlay
+
+closeBtn.addEventListener("click", function () {
+    sidebar.classList.remove("show-modal-overlay")
+})
+
+
