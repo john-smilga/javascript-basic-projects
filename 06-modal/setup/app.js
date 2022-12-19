499cbff69e7e77260e9e06cbb3d6f2e980f2e5bd
luisantos1986 <ing.luisantos@gmail.com>
2022-12-19 06:45:03 -0600

    toggling open-modal class

--- 06-modal/setup/app.js
+++ 06-modal/setup/app.js
@@ -7,27 +7,9 @@ const modalContainer = document.querySelector('.modal-container')
 
 // listen for click events on modal-btn and close-btn
 modalBtn.addEventListener("click", function () {
-    console.log (modalBtn)
-    modalBtn.classList.add("show-modalContainer")
+    modalOverlay.classList.toggle('open-modal')
 });
 
 closeBtn.addEventListener("click", function () {
-    modalOverlay.classList.remove(show-modalOverlay)
+    modalOverlay.classList.toggle('open-modal')
 })
-
-// when user clicks modal-btn add .open-modal to modal-overlay
-if (modalOverlay.classList.contains('show-modalOverlay')) {
-    modalOverlay.classList.remove('show-modalOverlay')
-}
-else {
-modalOverlay.classList.add("show-modal-overlay")
-}
-
-
-// when user clicks close-btn remove .open-modal from modal-overlay
-
-closeBtn.addEventListener("click", function () {
-    sidebar.classList.remove("show-modal-overlay")
-})
-
-
