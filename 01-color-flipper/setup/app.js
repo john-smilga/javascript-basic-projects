7609b6fe1cc9b8297c2a72d1f6f5aaddfabf4571
Mareedu Saibabu <mareedu.saibabu@publicissapient.com>
2023-01-12 19:17:20 +0530

    Color Flipper

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,15 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ['green', 'red', 'rgba(133,122,200)', '#f15025'];
+
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
+function changeColor() {
+  let randomNumber = getRandomNumber();
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+}
+
+btn.addEventListener('click', changeColor);
--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,18 @@
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+function getRandomColor() {
+  return Math.floor(Math.random() * hex.length);
+}
+function changeColor() {
+  let hexColor = '#';
+  for (i = 0; i < 6; i++) {
+    hexColor += hex[getRandomColor()];
+  }
+  document.body.style.backgroundColor = hexColor;
+  color.textContent = hexColor;
+}
+
+btn.addEventListener('click', changeColor);
