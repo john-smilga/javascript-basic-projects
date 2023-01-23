bbb6d33827f29e465bbc0654fbc5167771fd7bd4
Neil1977 <Neil_lunn1977@hotmail.com>
2023-01-23 11:44:13 +0000

    slider and video controls added

--- 09-video/setup/app.js
+++ 09-video/setup/app.js
@@ -1,3 +1,16 @@
 // MDN
 // The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
 // The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images.
+
+const btn = document.querySelector(".switch-btn");
+const video = document.querySelector(".video-container");
+
+btn.addEventListener("click", function () {
+  if (!btn.classList.contains("slide")) {
+    btn.classList.add("slide");
+    video.pause();
+  } else {
+    btn.classList.remove("slide");
+    video.play();
+  }
+});
