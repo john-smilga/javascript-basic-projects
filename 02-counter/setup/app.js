cad808ca8e31263cd7bd86ef5b3162c0e415ae45
Neil1977 <Neil_lunn1977@hotmail.com>
2023-01-23 10:35:34 +0000

    Decrease button working

---
+++ 02-counter/setup/app.js
@@ -0,0 +1,16 @@
+// set initial count
+let count = 0;
+
+// select value and buttons
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("decrease")) {
+      count--;
+    }
+    value.textContent = count;
+  });
+});
