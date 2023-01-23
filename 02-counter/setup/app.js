6fa6a0dc5daa1edb71940c061dc204d060f6b487
Neil1977 <Neil_lunn1977@hotmail.com>
2023-01-23 10:43:09 +0000

    all buttons added and color on selection

--- 02-counter/setup/app.js
+++ 02-counter/setup/app.js
@@ -10,6 +10,19 @@ btns.forEach(function (btn) {
     const styles = e.currentTarget.classList;
     if (styles.contains("decrease")) {
       count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else {
+      count = 0;
+    }
+    if (count > 0) {
+      value.style.color = "green";
+    }
+    if (count < 0) {
+      value.style.color = "red";
+    }
+    if (count === 0) {
+      value.style.color = "#222";
     }
     value.textContent = count;
   });
