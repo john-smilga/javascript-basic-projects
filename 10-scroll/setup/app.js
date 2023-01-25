4e4afc62abe4efe9451fdbf2e5318eeb4c444da2
Neil1977 <Neil_lunn1977@hotmail.com>
2023-01-25 14:08:43 +0000

    added show links and close links

--- 10-scroll/setup/app.js
+++ 10-scroll/setup/app.js
@@ -4,8 +4,16 @@
 //offsetTop - A Number, representing the top position of the element, in pixels
 
 // ********** set date ************
-
+const date = document.getElementById("date");
+date.innerHTML = new Date().getFullYear();
 // ********** close links ************
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+  linksContainer.classList.toggle("show-links");
+});
 
 // ********** fixed navbar ************
 
