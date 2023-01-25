90bbabaef89330238900043c869a427623d60546
Neil1977 <109810029+Neil1977@users.noreply.github.com>
2023-01-25 10:43:16 +0000

    Add files via upload

---
+++ 10-scroll/setup/app.js
@@ -0,0 +1,13 @@
+// Element.getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
+// pageYOffset is a read - only window property that returns the number of pixels the document has been scrolled vertically.
+// slice extracts a section of a string without modifying original string
+//offsetTop - A Number, representing the top position of the element, in pixels
+
+// ********** set date ************
+
+// ********** close links ************
+
+// ********** fixed navbar ************
+
+// ********** smooth scroll ************
+// select links
