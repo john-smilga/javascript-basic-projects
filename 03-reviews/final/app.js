6c7e27fd3d1d68007bce0cf3c105d14750d780dd
john smilga <johnsmilga@gmail.com>
2023-01-29 10:27:07 -0500

    update to imgbox

--- 03-reviews/final/app.js
+++ 03-reviews/final/app.js
@@ -2,56 +2,48 @@
 const reviews = [
   {
     id: 1,
-    name: "susan smith",
-    job: "web developer",
-    img:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
-    text:
-      "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+    name: 'susan smith',
+    job: 'web developer',
+    img: 'https://images2.imgbox.com/e0/57/qI5bbwvg_o.jpeg',
+    text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
   },
   {
     id: 2,
-    name: "anna johnson",
-    job: "web designer",
-    img:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
-    text:
-      "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+    name: 'anna johnson',
+    job: 'web designer',
+    img: 'https://images2.imgbox.com/2e/6e/JAMvTZ56_o.jpeg',
+    text: 'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
   },
   {
     id: 3,
-    name: "peter jones",
-    job: "intern",
-    img:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
-    text:
-      "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+    name: 'peter jones',
+    job: 'intern',
+    img: 'https://images2.imgbox.com/56/88/oJvFN3l5_o.jpeg',
+    text: 'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
   },
   {
     id: 4,
-    name: "bill anderson",
-    job: "the boss",
-    img:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
-    text:
-      "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+    name: 'bill anderson',
+    job: 'the boss',
+    img: 'https://images2.imgbox.com/8b/1c/vwWNTsCd_o.jpeg',
+    text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
   },
 ];
 // select items
-const img = document.getElementById("person-img");
-const author = document.getElementById("author");
-const job = document.getElementById("job");
-const info = document.getElementById("info");
+const img = document.getElementById('person-img');
+const author = document.getElementById('author');
+const job = document.getElementById('job');
+const info = document.getElementById('info');
 
-const prevBtn = document.querySelector(".prev-btn");
-const nextBtn = document.querySelector(".next-btn");
-const randomBtn = document.querySelector(".random-btn");
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+const randomBtn = document.querySelector('.random-btn');
 
 // set starting item
 let currentItem = 0;
 
 // load initial item
-window.addEventListener("DOMContentLoaded", function () {
+window.addEventListener('DOMContentLoaded', function () {
   const item = reviews[currentItem];
   img.src = item.img;
   author.textContent = item.name;
@@ -68,7 +60,7 @@ function showPerson(person) {
   info.textContent = item.text;
 }
 // show next person
-nextBtn.addEventListener("click", function () {
+nextBtn.addEventListener('click', function () {
   currentItem++;
   if (currentItem > reviews.length - 1) {
     currentItem = 0;
@@ -76,7 +68,7 @@ nextBtn.addEventListener("click", function () {
   showPerson(currentItem);
 });
 // show prev person
-prevBtn.addEventListener("click", function () {
+prevBtn.addEventListener('click', function () {
   currentItem--;
   if (currentItem < 0) {
     currentItem = reviews.length - 1;
@@ -84,8 +76,8 @@ prevBtn.addEventListener("click", function () {
   showPerson(currentItem);
 });
 // show random person
-randomBtn.addEventListener("click", function () {
-  console.log("hello");
+randomBtn.addEventListener('click', function () {
+  console.log('hello');
 
   currentItem = Math.floor(Math.random() * reviews.length);
   showPerson(currentItem);
--- 25-slider/setup/data.js
+++ 25-slider/setup/data.js
@@ -1,23 +1,20 @@
 const people = [
   {
-    img:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/c_scale,w_200/v1595959121/person-1_aufeoq.jpg",
-    name: "peter doe",
-    job: "product manager",
+    img: 'https://images2.imgbox.com/56/88/oJvFN3l5_o.jpeg',
+    name: 'peter doe',
+    job: 'product manager',
     text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis? `,
   },
   {
-    img:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/c_scale,w_200/v1595959131/person-2_ipcjws.jpg",
-    name: "susan doe",
-    job: "developer",
+    img: 'https://images2.imgbox.com/e0/57/qI5bbwvg_o.jpeg',
+    name: 'susan doe',
+    job: 'developer',
     text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
   },
   {
-    img:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/c_scale,w_200/v1595959131/person-3_rxtqvi.jpg",
-    name: "emma doe",
-    job: "designer",
+    img: 'https://images2.imgbox.com/2e/6e/JAMvTZ56_o.jpeg',
+    name: 'emma doe',
+    job: 'designer',
     text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
   },
 ];
