6f4c947a8493172b2afeb915a432c3c5ab61ad71
john smilga <johnsmilga@gmail.com>
2023-04-29 21:30:20 -0400

    fixed images

--- 20-filters/final/products.js
+++ 20-filters/final/products.js
@@ -4,7 +4,7 @@ const products = [
     title: 'high-back bench',
     company: 'ikea',
     image:
-      'https://dl.airtable.com/.attachments/14ac9e946e1a02eb9ce7d632c83f742f/4fd98e64/product-1.jpeg',
+      'https://res.cloudinary.com/dt2g7mgtv/image/upload/v1681749482/react-comfy-store-products/iuYyO9RP_o_upinxq.jpg',
     price: 9.99,
   },
   {
@@ -12,7 +12,7 @@ const products = [
     title: 'albany table',
     company: 'marcos',
     image:
-      'https://dl.airtable.com/.attachments/f3450755e165557344f7d6433242431e/93533098/product-1.jpeg',
+      'https://res.cloudinary.com/dt2g7mgtv/image/upload/v1681750782/react-comfy-store-products/product-2_lusrzx.jpg',
     price: 79.99,
   },
   {
@@ -20,7 +20,7 @@ const products = [
     title: 'accent chair',
     company: 'caressa',
     image:
-      'https://dl.airtable.com/.attachments/f292eaec4fea8a791b037c759ae559c9/2d5e34d5/product-4.jpeg',
+      'https://res.cloudinary.com/dt2g7mgtv/image/upload/v1681750874/react-comfy-store-products/product-3_znpiqa.jpg',
     price: 25.99,
   },
   {
@@ -28,7 +28,8 @@ const products = [
     title: 'wooden table',
     company: 'caressa',
     image:
-      'https://dl.airtable.com/.attachments/5b3ad76dfd6ca5e31810cb99141c7ede/69829b2f/pexels-dominika-roseclay-1139785.jpg',
+      'https://res.cloudinary.com/dt2g7mgtv/image/upload/v1681750929/react-comfy-store-products/product-4_ebl6q1.jpg',
+
     price: 45.99,
   },
   {
@@ -36,7 +37,8 @@ const products = [
     title: 'dining table',
     company: 'caressa',
     image:
-      'https://dl.airtable.com/.attachments/06bbede65bd09196b4a81a8e7b59e683/8af0aeb5/product-8.jpg',
+      'https://res.cloudinary.com/dt2g7mgtv/image/upload/v1681751026/react-comfy-store-products/product-5_n184nu.jpg',
+
     price: 6.99,
   },
   {
@@ -44,7 +46,7 @@ const products = [
     title: 'sofa set',
     company: 'liddy',
     image:
-      'https://dl.airtable.com/.attachments/443aa7884207dae7c3cc127262a2f993/d4f33110/product-1.jpeg',
+      'https://res.cloudinary.com/dt2g7mgtv/image/upload/v1681752461/react-comfy-store-products/product-6_rhaxfo.jpg',
     price: 69.99,
   },
   {
@@ -52,7 +54,7 @@ const products = [
     title: 'modern bookshelf',
     company: 'marcos',
     image:
-      'https://dl.airtable.com/.attachments/a889b1928b134c2ca0b5bbca32ea9abf/65c8a9a6/product-7.jpg',
+      'https://res.cloudinary.com/dt2g7mgtv/image/upload/v1681753280/react-comfy-store-products/prod-7_ta21yy.jpg',
     price: 8.99,
   },
   {
@@ -60,7 +62,7 @@ const products = [
     title: 'emperor bed',
     company: 'liddy',
     image:
-      'https://dl.airtable.com/.attachments/10fdf29ae17f2d1f98770ae42584d021/82b9403f/product-6.jpg',
+      'https://res.cloudinary.com/dt2g7mgtv/image/upload/v1681753314/react-comfy-store-products/prod-8_qzulqi.jpg',
     price: 21.99,
   },
   {
@@ -68,7 +70,7 @@ const products = [
     title: 'utopia sofa',
     company: 'marcos',
     image:
-      'https://dl.airtable.com/.attachments/6ac7f7b55d505057317534722e5a9f03/9183491e/product-3.jpg',
+      'https://res.cloudinary.com/dt2g7mgtv/image/upload/v1681753354/react-comfy-store-products/prod-9_j3dsja.jpg',
     price: 39.95,
   },
   {
@@ -76,7 +78,7 @@ const products = [
     title: 'entertainment center',
     company: 'liddy',
     image:
-      'https://dl.airtable.com/.attachments/da5e17fd71f50578d525dd5f596e407e/d5e88ac8/product-2.jpg',
+      'https://res.cloudinary.com/dt2g7mgtv/image/upload/v1681753394/react-comfy-store-products/prod-10_d0jqoh.jpg',
     price: 29.98,
   },
   {
@@ -84,7 +86,7 @@ const products = [
     title: 'albany sectional',
     company: 'ikea',
     image:
-      'https://dl.airtable.com/.attachments/05ecddf7ac8d581ecc3f7922415e7907/a4242abc/product-1.jpeg',
+      'https://res.cloudinary.com/dt2g7mgtv/image/upload/v1681753441/react-comfy-store-products/prod-11_lff6yt.jpg',
     price: 10.99,
   },
   {
@@ -92,7 +94,7 @@ const products = [
     title: 'leather sofa',
     company: 'liddy',
     image:
-      'https://dl.airtable.com/.attachments/3245c726ee77d73702ba8c3310639727/f000842b/product-5.jpg',
+      'https://res.cloudinary.com/dt2g7mgtv/image/upload/v1681753501/react-comfy-store-products/prod-12_ga6sdq.jpg',
     price: 9.99,
   },
 ];
