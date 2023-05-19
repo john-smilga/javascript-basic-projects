ff3cdf1fb023f2aba73fed37b56721ec6114f11b
john smilga <johnsmilga@gmail.com>
2023-05-19 13:58:53 -0400

    switched 25 slider to local images

--- 25-slider/setup/data.js
+++ 25-slider/setup/data.js
@@ -1,20 +1,22 @@
 const people = [
   {
-    img: 'https://images2.imgbox.com/56/88/oJvFN3l5_o.jpeg',
+    img: 'https://www.course-api.com/images/people/person-3.jpeg',
     name: 'peter doe',
     job: 'product manager',
     text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis? `,
   },
   {
-    img: 'https://images2.imgbox.com/e0/57/qI5bbwvg_o.jpeg',
+    img: 'https://www.course-api.com/images/people/person-1.jpeg',
     name: 'susan doe',
     job: 'developer',
     text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
   },
   {
-    img: 'https://images2.imgbox.com/2e/6e/JAMvTZ56_o.jpeg',
+    img: 'https://www.course-api.com/images/people/person-2.jpeg',
     name: 'emma doe',
     job: 'designer',
     text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
   },
 ];
+
+export default people;
