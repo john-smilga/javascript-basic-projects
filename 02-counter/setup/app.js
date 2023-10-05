77588531fc8ca28d31d79f2832a62d5478e305dd
Lisa Takenouchi <rhrbanb@gmail.com>
2023-10-05 16:07:33 +0900

    "completed the counter"

---
+++ 02-counter/setup/app.js
@@ -0,0 +1,30 @@
+// set initial count
+let count = 0;
+
+// select value and button
+
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+    if(styles.contains('decrease')){
+      count--;
+    }else if(styles.contains('increase')){
+      count++;
+    }else{
+      count = 0;
+    }
+    if(count > 0){
+      value.style.color = 'green';
+    }
+    if(count < 0){
+      value.style.color = "blue";
+    }
+    if(count === 0){
+      value.style.color = "#222";
+    }
+    value.textContent = count;
+  });
+});
