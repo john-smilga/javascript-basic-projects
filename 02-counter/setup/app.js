5b369dfa4e77bab1bd2bbc1f3f4927cc4b4e7380
Lisa Takenouchi <rhrbanb@gmail.com>
2023-10-05 16:09:44 +0900

    added

--- 02-counter/setup/app.js
+++ 02-counter/setup/app.js
@@ -5,7 +5,7 @@ let count = 0;
 
 const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
-
+// btn
 btns.forEach(function (btn) {
   btn.addEventListener("click", function (e) {
     const styles = e.currentTarget.classList;
