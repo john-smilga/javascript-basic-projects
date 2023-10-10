676d3e38fa6ce0bed4da665a61e5b691ffe4f884
SavviSam <swghazzawi@gmail.com>
2023-10-10 11:05:43 -0600

    color flipper project

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,27 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.querySelector("button");
+const color = document.querySelector(".color");
+const navLink = document.querySelectorAll("nav a");
+const activePage = window.location.pathname;
+
+navLink.forEach((link) => {
+  if (link.href.includes(`${activePage}`)) {
+    link.classList.add("active");
+  }
+});
+
+// document bg color matches color on page load
+window.onload = () => {
+  document.body.style.backgroundColor = color.textContent;
+};
+
+btn.addEventListener("click", () => {
+  // get random number between 0 - 3
+  const randomNumber = getRandomNumber();
+  document.body.style.backgroundColor = randomNumber;
+  color.textContent = randomNumber;
+});
+
+const getRandomNumber = () => {
+  return colors[Math.floor(Math.random() * colors.length)];
+};
--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,10 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const navLink = document.querySelectorAll("nav a");
+const activePage = window.location.pathname;
+
+navLink.forEach((link) => {
+  if (link.href.includes(`${activePage}`)) {
+    link.classList.add("active");
+  }
+});
