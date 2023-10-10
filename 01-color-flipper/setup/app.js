8663214901658edec6992511acf392ac08263fc5
SavviSam <swghazzawi@gmail.com>
2023-10-10 16:16:40 -0600

    complete background color generator

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -2,26 +2,24 @@ const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 const btn = document.querySelector("button");
 const color = document.querySelector(".color");
 const navLink = document.querySelectorAll("nav a");
-const activePage = window.location.pathname;
-
-navLink.forEach((link) => {
-  if (link.href.includes(`${activePage}`)) {
-    link.classList.add("active");
-  }
-});
 
 // document bg color matches color on page load
 window.onload = () => {
   document.body.style.backgroundColor = color.textContent;
 };
 
-btn.addEventListener("click", () => {
-  // get random number between 0 - 3
-  const randomNumber = getRandomNumber();
+const getRandomNumber = () => {
+  const randomNumber = colors[Math.floor(Math.random() * colors.length)];
   document.body.style.backgroundColor = randomNumber;
   color.textContent = randomNumber;
-});
-
-const getRandomNumber = () => {
-  return colors[Math.floor(Math.random() * colors.length)];
 };
+
+btn.addEventListener("click", getRandomNumber);
+
+// highlight nav-link for current page
+const activePage = window.location.pathname;
+navLink.forEach((link) => {
+  if (link.href.includes(`${activePage}`)) {
+    link.classList.add("active");
+  }
+});
--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,8 +1,25 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.querySelector("button");
+const color = document.querySelector(".color");
 
+window.onload = () => {
+  document.body.style.backgroundColor = color.textContent;
+};
+
+const getRandomHex = () => {
+  let hexColor = "#";
+  for (let i = 0; i < 6; i++) {
+    hexColor += hex[Math.floor(Math.random() * hex.length)];
+  }
+  color.textContent = hexColor;
+  document.body.style.backgroundColor = hexColor;
+};
+
+btn.addEventListener("click", getRandomHex);
+
+//highlight nav link for current page
 const navLink = document.querySelectorAll("nav a");
 const activePage = window.location.pathname;
-
 navLink.forEach((link) => {
   if (link.href.includes(`${activePage}`)) {
     link.classList.add("active");
