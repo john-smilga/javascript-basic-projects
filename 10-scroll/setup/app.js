8ac6ae1e531b7b76762e8cab7301ae3aac0fabb8
SavviSam <swghazzawi@gmail.com>
2023-10-17 16:02:43 -0600

    completed scroll project

--- 10-scroll/setup/app.js
+++ 10-scroll/setup/app.js
@@ -12,17 +12,54 @@ const navToggle = document.querySelector(".nav-toggle");
 const linksContainer = document.querySelector(".links-container");
 const links = document.querySelector(".links");
 
-navToggle.addEventListener("click", () => {
-  // linksContainer.classList.toggle("show-links");
+function toggleLinks() {
   const containerHeight = linksContainer.getBoundingClientRect().height;
   const linksHeight = links.getBoundingClientRect().height;
-  console.log(linksHeight);
 
   containerHeight === 0
     ? (linksContainer.style.height = `${linksHeight}px`)
     : (linksContainer.style.height = 0);
-});
+}
+navToggle.addEventListener("click", toggleLinks);
+
 // ********** fixed navbar ************
+const navbar = document.querySelector("#nav");
+const topLink = document.querySelector(".top-link");
+
+function fixedNav() {
+  const scrollHeight = window.scrollY;
+  const navHeight = navbar.getBoundingClientRect().height;
+
+  scrollHeight > navHeight
+    ? nav.classList.add("fixed-nav") || topLink.classList.add("show-link")
+    : nav.classList.remove("fixed-nav") ||
+      topLink.classList.remove("show-link");
+}
+window.addEventListener("scroll", fixedNav);
 
 // ********** smooth scroll ************
 // select links
+const scrollLinks = document.querySelectorAll(".scroll-link ");
+
+const smoothScroll = (e) => {
+  // prevent the default scroll setting
+  e.preventDefault();
+  //navigate to specific spot
+  const targetID = e.currentTarget.getAttribute("href").slice(1);
+  const targetElement = document.getElementById(targetID);
+
+  //calculate heights
+  const navHeight = navbar.getBoundingClientRect().height;
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  fixedNav = navbar.classList.contains("fixed-nav");
+
+  let position = targetElement.offsetTop - navHeight;
+  if (!fixedNav) position = position - navHeight;
+  if (navHeight > 82) position = position + containerHeight;
+
+  window.scrollTo({ left: 0, top: position, behavior: "smooth" });
+  linksContainer.style.height = 0;
+};
+scrollLinks.forEach((link) => {
+  link.addEventListener("click", smoothScroll);
+});
