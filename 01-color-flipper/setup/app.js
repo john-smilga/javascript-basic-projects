d1ecc9851fb978712c87ab25ba1eaf179aac0048
Pranay-116 <kumarpusp2000@gmail.coms>
2023-11-26 11:36:39 +0530

    initial commit

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,17 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+
+btn.addEventListener('click',function(){
+   // get random number between 0-3 colors[3]
+
+   const randomNumber = getRandomNumber();
+   document.body.style.backgroundColor=colors[randomNumber]
+   color.textContent = colors[randomNumber]
+});
+
+function getRandomNumber() {
+    return Math.floor(Math.random()*colors.length);
+}
+
