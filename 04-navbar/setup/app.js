1f703b3abd377127ac5b8424d581f362ddc451c1
Pranay-116 <kumarpusp2000@gmail.coms>
2023-11-27 00:03:46 +0530

    navbar commit

--- 04-navbar/setup/app.js
+++ 04-navbar/setup/app.js
@@ -3,3 +3,18 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const btn = document.querySelector('.nav-toggle');
+const lnks = document.querySelector('.links');
+
+btn.addEventListener('click',function(){
+    // if(lnks.classList.contains('show-links'))
+    // {
+    //    lnks.classList.remove('show-links');
+    // }
+    // else
+    // {
+    //     lnks.classList.add('show-links');
+    // }
+    lnks.classList.toggle('show-links');
+})
