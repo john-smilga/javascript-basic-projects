6823833a3675080c6e78a26a95a272ab80742ac9
Sultan <sbaratahunov@gmail.com>
2024-02-03 11:33:46 +0600

    completed  navbar

--- 04-navbar/setup/app.js
+++ 04-navbar/setup/app.js
@@ -3,3 +3,14 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const links = document.querySelector(".links");
+const toggleBtn = document.querySelector(".nav-toggle");
+
+toggleBtn.addEventListener("click", () => {
+  if (!links.classList.contains("show-links")) {
+    links.classList.add("show-links");
+  } else {
+    links.classList.remove("show-links");
+  }
+});
